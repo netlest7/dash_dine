8c3100416e241a1dcbf07b3d929746ee2bbff673
Netlest <162489688+netlest7@users.noreply.github.com>
2024-05-04 15:16:55 +0530

    Update index.ts cors url

--- backend/index.ts
+++ backend/index.ts
@@ -18,7 +18,7 @@ async function startServer() {
 app.use(express.json());
 app.use(cookieparser())
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://dash-dine-8bf6d.web.app'],
     credentials: true
 }))
 
