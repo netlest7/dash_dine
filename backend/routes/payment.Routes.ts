541b8c93e0b48bec934cd5e48f495525ded3cc20
Netlest <162489688+netlest7@users.noreply.github.com>
2024-05-04 14:49:40 +0530

    Update payment.Routes.ts

--- backend/routes/payment.Routes.ts
+++ backend/routes/payment.Routes.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import { isAuthenticatedUser } from "../middleware/auth";
+import { createAndBuySubscription, paymentVerification } from "../controllers/payment.Controllers";
 const paymentRouter = express.Router();
 
 
-paymentRouter.post('/createSubscription',isAuthenticatedUser,)
+paymentRouter.post('/createSubscription',isAuthenticatedUser,createAndBuySubscription)
+
+paymentRouter.post('/paymentVerification',isAuthenticatedUser,paymentVerification)
 
 
 
